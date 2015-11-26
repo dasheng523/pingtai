71a9ab8a45d182792ad960a33983c591e5275f8d
dasheng523 <dasheng523@163.com>
2015-11-26 08:21:39 +0800

    gitigore profiles.clj

---
+++ profiles.clj
@@ -0,0 +1,7 @@
+{:profiles/dev  {:env {:db-subname "//localhost:3306/pingtai_dev"
+                       :db-user "admin"
+                       :db-password "123456"}}
+
+ :profiles/test {:env {:db-subname "//localhost:3306/pingtai_dev"
+                       :db-user "admin"
+                       :db-password "123456"}}}
