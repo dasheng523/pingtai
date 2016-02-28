cc927fe23cb8d0a0a2ee1f1b341238f0f5a7b5a9
dasheng523 <dasheng523@163.com>
2016-02-28 18:00:50 +0800

    加入一些静态页面

---
+++ Application/Phone/Controller/PublicController.class.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yesheng
+ * Date: 16-2-21
+ * Time: 上午10:23
+ */
+
+namespace Phone\Controller;
+class PublicController extends OauthController {
+
+
+}
