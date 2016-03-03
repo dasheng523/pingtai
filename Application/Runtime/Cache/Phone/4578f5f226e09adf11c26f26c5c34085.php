ca28bd2d4213a27e5b26f83cf95f8ff0635bb79a
dasheng523 <dasheng523@163.com>
2016-03-03 11:12:46 +0800

    加入了商品详情页，并修改了一些BUG

--- Application/Runtime/Cache/Phone/4578f5f226e09adf11c26f26c5c34085.php
+++ Application/Runtime/Cache/Phone/4578f5f226e09adf11c26f26c5c34085.php
@@ -12,22 +12,22 @@
     <link rel="stylesheet" href="http://192.168.23.105/pingtai/Public/SUI-Mobile/dist/css/sm.min.css">
     <link rel="stylesheet" href="//g.alicdn.com/msui/sm/0.6.2/css/sm-extend.min.css">
     <link rel="stylesheet" href="http://192.168.23.105/pingtai/Public/css/weui.min.css">
-    <link rel="stylesheet" href="http://192.168.23.105/pingtai/Public/css/main.css?v=13">
+    <link rel="stylesheet" href="http://192.168.23.105/pingtai/Public/css/main.css?v=15">
 
 </head>
 <body>
 <div class="page-group">
     <div class="page page-current" id="collection-info">
-        <div class="bar">
-            <div class="searchbar">
-                <a class="searchbar-cancel">取消</a>
-                <div class="search-input">
-                    <label class="icon icon-search" for="search"></label>
-                    <input type="search" id="search" placeholder="输入关键字...">
+        <div class="content native-scroll">
+            <div class="bar">
+                <div class="searchbar">
+                    <a class="searchbar-cancel">取消</a>
+                    <div class="search-input">
+                        <label class="icon icon-search" for="search"></label>
+                        <input type="search" id="search" placeholder="输入关键字...">
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="content native-scroll">
             <div class="swiper-container adbox" data-space-between='10'>
                 <div class="swiper-wrapper">
                     <div class="swiper-slide"><img src="http://192.168.23.105/pingtai/Public/images/banner.jpg" alt="" style='width: 100%'></div>
@@ -121,50 +121,56 @@
 
 
                 <div id="tab2" class="tab">
-                    <div class="card facebook-card">
-                        <div class="card-header no-border">
-                            <div class="facebook-avatar"><img src="http://gqianniu.alicdn.com/bao/uploaded/i4//tfscom/i3/TB10LfcHFXXXXXKXpXXXXXXXXXX_!!0-item_pic.jpg_250x250q60.jpg" width="34" height="34"></div>
-                            <div class="facebook-name">美女大集合</div>
-                            <div class="facebook-date">by 夜萧 <span class="fr red-text" style="font-size: 0.5rem;">热门度 80%</span></div>
-                        </div>
-                        <div class="card-content">
-                            <div class="card-text">
-                                我们对页面的基本结构是有要求的，否则可能会出现样式错误或者无法正确加载页面等问题我们对页面的基本结构是有要求的，否则可能会出现样式错误或者无法正确加载页面等问题
+                    <div class="card facebook-card list-block">
+                        <a class="item-link" href="<?php echo UC('Index/collection');?>">
+                            <div class="card-header no-border">
+                                <div class="facebook-avatar"><img src="http://gqianniu.alicdn.com/bao/uploaded/i4//tfscom/i3/TB10LfcHFXXXXXKXpXXXXXXXXXX_!!0-item_pic.jpg_250x250q60.jpg" width="34" height="34"></div>
+                                <div class="facebook-name">美女大集合</div>
+                                <div class="facebook-date">by 夜萧 <span class="fr red-text" style="font-size: 0.5rem;">热门度 80%</span></div>
                             </div>
-                            <div class="card-pic">
-                                <div class="row no-gutter">
-                                    <div class="col-25"><img src="http://192.168.23.105/pingtai/Public/images/head.jpg"></div>
-                                    <div class="col-25"><img src="http://192.168.23.105/pingtai/Public/images/head.jpg"></div>
-                                    <div class="col-25"><img src="http://192.168.23.105/pingtai/Public/images/head.jpg"></div>
-                                    <div class="col-25"><img src="http://192.168.23.105/pingtai/Public/images/head.jpg"></div>
+                            <div class="card-content">
+                                <div class="card-text">
+                                    我们对页面的基本结构是有要求的，否则可能会出现样式错误或者无法正确加载页面等问题我们对页面的基本结构是有要求的，否则可能会出现样式错误或者无法正确加载页面等问题
+                                </div>
+                                <div class="card-pic">
+                                    <div class="row no-gutter">
+                                        <div class="col-25"><img src="http://192.168.23.105/pingtai/Public/images/head.jpg"></div>
+                                        <div class="col-25"><img src="http://192.168.23.105/pingtai/Public/images/head.jpg"></div>
+                                        <div class="col-25"><img src="http://192.168.23.105/pingtai/Public/images/head.jpg"></div>
+                                        <div class="col-25"><img src="http://192.168.23.105/pingtai/Public/images/head.jpg"></div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
+                        </a>
+
                         <div class="card-footer no-border">
                             <a href="#" class="link">赞一个</a>
                             <a href="#" class="link">分享</a>
                         </div>
                     </div>
 
-                    <div class="card facebook-card">
-                        <div class="card-header no-border">
-                            <div class="facebook-avatar"><img src="http://gqianniu.alicdn.com/bao/uploaded/i4//tfscom/i3/TB10LfcHFXXXXXKXpXXXXXXXXXX_!!0-item_pic.jpg_250x250q60.jpg" width="34" height="34"></div>
-                            <div class="facebook-name">美女大集合</div>
-                            <div class="facebook-date">by 夜萧 <span class="fr red-text" style="font-size: 0.5rem;">热门度 80%</span></div>
-                        </div>
-                        <div class="card-content">
-                            <div class="card-text">
-                                我们对页面的基本结构是有要求的，否则可能会出现样式错误或者无法正确加载页面等问题我们对页面的基本结构是有要求的，否则可能会出现样式错误或者无法正确加载页面等问题
+                    <div class="card facebook-card list-block">
+                        <a class="item-link" href="<?php echo UC('Index/collection');?>">
+                            <div class="card-header no-border">
+                                <div class="facebook-avatar"><img src="http://gqianniu.alicdn.com/bao/uploaded/i4//tfscom/i3/TB10LfcHFXXXXXKXpXXXXXXXXXX_!!0-item_pic.jpg_250x250q60.jpg" width="34" height="34"></div>
+                                <div class="facebook-name">美女大集合</div>
+                                <div class="facebook-date">by 夜萧 <span class="fr red-text" style="font-size: 0.5rem;">热门度 80%</span></div>
                             </div>
-                            <div class="card-pic">
-                                <div class="row no-gutter">
-                                    <div class="col-25"><img src="http://192.168.23.105/pingtai/Public/images/head.jpg"></div>
-                                    <div class="col-25"><img src="http://192.168.23.105/pingtai/Public/images/head.jpg"></div>
-                                    <div class="col-25"><img src="http://192.168.23.105/pingtai/Public/images/head.jpg"></div>
-                                    <div class="col-25"><img src="http://192.168.23.105/pingtai/Public/images/head.jpg"></div>
+                            <div class="card-content">
+                                <div class="card-text">
+                                    我们对页面的基本结构是有要求的，否则可能会出现样式错误或者无法正确加载页面等问题我们对页面的基本结构是有要求的，否则可能会出现样式错误或者无法正确加载页面等问题
+                                </div>
+                                <div class="card-pic">
+                                    <div class="row no-gutter">
+                                        <div class="col-25"><img src="http://192.168.23.105/pingtai/Public/images/head.jpg"></div>
+                                        <div class="col-25"><img src="http://192.168.23.105/pingtai/Public/images/head.jpg"></div>
+                                        <div class="col-25"><img src="http://192.168.23.105/pingtai/Public/images/head.jpg"></div>
+                                        <div class="col-25"><img src="http://192.168.23.105/pingtai/Public/images/head.jpg"></div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
+                        </a>
+
                         <div class="card-footer no-border">
                             <a href="#" class="link">赞一个</a>
                             <a href="#" class="link">分享</a>
@@ -178,6 +184,7 @@
                 <!-- footer end -->
 
             </div>
+            <div style="height: 50px;"></div>
         </div>
         <nav class="bar bar-tab">
             <a class="tab-item external active" href="#">
@@ -185,17 +192,17 @@
                 <span class="tab-label">首页</span>
             </a>
             <a class="tab-item external" href="#">
-                <span class="icon icon-me"></span>
-                <span class="tab-label">Label</span>
+                <span class="icon"><i class="iconfont">&#xe604;</i></span>
+                <span class="tab-label">发布</span>
+            </a>
+            <a class="tab-item external" href="#">
+                <span class="icon"><i class="iconfont">&#xe605;</i></span>
+                <span class="tab-label">排行榜</span>
+            </a>
+            <a class="tab-item external" href="#">
+                <span class="icon"><i class="iconfont">&#xe603;</i></span>
+                <span class="tab-label">个人中心</span>
                 <span class="badge">2</span>
-            </a>
-            <a class="tab-item external" href="#">
-                <span class="icon icon-star"></span>
-                <span class="tab-label">Label</span>
-            </a>
-            <a class="tab-item external" href="#">
-                <span class="icon icon-cart"></span>
-                <span class="tab-label">Label</span>
             </a>
         </nav>
         <div class="footer footer-menu">
