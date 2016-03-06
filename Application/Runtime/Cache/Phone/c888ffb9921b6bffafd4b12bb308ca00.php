39391a93936dbdc88e3e089888c097e18dd9b5ac
dasheng523 <dasheng523@163.com>
2016-03-06 19:15:01 +0800

    完成静态页面

--- Application/Runtime/Cache/Phone/c888ffb9921b6bffafd4b12bb308ca00.php
+++ Application/Runtime/Cache/Phone/c888ffb9921b6bffafd4b12bb308ca00.php
@@ -28,43 +28,122 @@
                 <div class="swiper-pagination"><span class="swiper-pagination-bullet swiper-pagination-bullet-active"></span><span class="swiper-pagination-bullet"></span><span class="swiper-pagination-bullet"></span></div>
             </div>
 
-            <div class="row content-block">
-                <div class="col-33">
-                    <a href="javascript:;" class="weui_btn weui_btn_primary">想买</a>
-                </div>
-                <div class="col-33">
-                    <a href="javascript:;" class="weui_btn weui_btn_warn">收藏</a>
-                </div>
-                <div class="col-33">
-                    <a href="javascript:;" class="weui_btn weui_btn_primary">分享</a>
-                </div>
-            </div>
-
             <div class="card">
                 <div class="card-content">
                     <div class="list-block">
                         <ul>
                             <li class="item-content">
                                 <div class="item-inner">
-                                    <div class="item-title">东门口何玲米粽</div>
+                                    <div class="item-title text-center" style="width: 100%;">何玲三角米粽</div>
                                 </div>
                             </li>
                             <li class="item-content">
                                 <div class="item-inner">
-                                    <span class="green-text big-text">
+                                    <div class="green-text big-text">
                                         <small class="gray-text small-text">市面价：</small>
-                                        ￥5
-                                    </span>
-                                    <small class="gray-text small-text fr">500人收藏 | 40人想买</small>
+                                        ￥50
+                                    </div>
                                 </div>
                             </li>
                         </ul>
                     </div>
+                    <div class="card-footer no-border">
+                        <a href="#" class="link">收藏(30)</a>
+                        <a href="#" class="link">想买(30)</a>
+                    </div>
                 </div>
             </div>
+            <div class="content-block-title">描述</div>
             <div class="card">
                 <div class="card-content">
                     <div class="card-content-inner">这是一个用纯文本的简单卡片。但卡片可以包含自己的页头，页脚，列表视图，图像，和里面的任何元素。</div>
+                </div>
+            </div>
+
+            <div class="content-block-title">商家信息</div>
+            <div class="card">
+                <div class="card-content">
+                    <div class="list-block media-list inset">
+                        <ul>
+                            <li>
+                                <a href="#" class="item-link item-content">
+                                    <div class="item-media"><img src="http://gqianniu.alicdn.com/bao/uploaded/i4//tfscom/i3/TB10LfcHFXXXXXKXpXXXXXXXXXX_!!0-item_pic.jpg_250x250q60.jpg" width="44"></div>
+                                    <div class="item-inner">
+                                        <div class="item-title-row">
+                                            <div class="item-title">东门口何玲米粽店</div>
+                                        </div>
+                                        <div class="item-subtitle">十字步行街往西门口方向右边摊位</div>
+                                    </div>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="card-footer"><a href="#" class="link">点击查看地图</a></div>
+            </div>
+
+
+            <div class="content-block-title">评论(50)<a href="#" class="fr">写评论</a></div>
+            <div class="card facebook-card list-block">
+                <div class="card-header no-border">
+                    <div class="facebook-avatar"><img src="http://gqianniu.alicdn.com/bao/uploaded/i4//tfscom/i3/TB10LfcHFXXXXXKXpXXXXXXXXXX_!!0-item_pic.jpg_250x250q60.jpg" width="34" height="34"></div>
+                    <div class="facebook-name">夜声</div>
+                    <div class="facebook-date">今天09:01</div>
+                </div>
+                <div class="card-content" style="margin-top: -15px;">
+                    <div class="card-content-inner">
+                        吃牙疼药最好，还要去牙科。吃牙疼药最好，还要去牙科。吃牙疼药最好，还要去牙科。吃牙疼药最好，还要去牙科。吃牙疼药最好，还要去牙科。
+                        <div class="comment-box flex" style="background: #eeeeee;">
+                            <img src="http://192.168.23.105/pingtai/Public/images/head.jpg" class="comment-box-head">
+                            <div class="comment-box-infobox flex-1">
+                                <h3 class="comment-box-name">夜声<a href="#" class="comment-box-replybtn">回复TA</a></h3>
+                                <span class="comment-box-time">今天15:30 </span>
+                                <div class="comment-box-content">
+                                    那里的牙医比较呢？
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card-footer no-border">
+                    <a href="#" class="link">回复</a>
+                    <a href="#" class="link">赞</a>
+                </div>
+            </div>
+            <div class="card facebook-card list-block">
+                <div class="card-header no-border">
+                    <div class="facebook-avatar"><img src="http://gqianniu.alicdn.com/bao/uploaded/i4//tfscom/i3/TB10LfcHFXXXXXKXpXXXXXXXXXX_!!0-item_pic.jpg_250x250q60.jpg" width="34" height="34"></div>
+                    <div class="facebook-name">夜声</div>
+                    <div class="facebook-date">今天09:01</div>
+                </div>
+                <div class="card-content" style="margin-top: -15px;">
+                    <div class="card-content-inner">
+                        吃牙疼药最好，还要去牙科。吃牙疼药最好，还要去牙科。吃牙疼药最好，还要去牙科。吃牙疼药最好，还要去牙科。吃牙疼药最好，还要去牙科。
+                        <div class="comment-box flex" style="background: #eeeeee;">
+                            <img src="http://192.168.23.105/pingtai/Public/images/head.jpg" class="comment-box-head">
+                            <div class="comment-box-infobox flex-1">
+                                <h3 class="comment-box-name">夜声<a href="#" class="comment-box-replybtn">回复TA</a></h3>
+                                <span class="comment-box-time">今天15:30 </span>
+                                <div class="comment-box-content">
+                                    那里的牙医比较呢？
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card-footer no-border">
+                    <a href="#" class="link">回复</a>
+                    <a href="#" class="link">赞</a>
+                </div>
+            </div>
+
+
+            <div style="height: 50px;"></div>
+            <div class="fix-bottom row no-gutter">
+                <div class="col-100">
+                    <a href="#" class="button button-big button-fill button-warning">联系店主</a>
                 </div>
             </div>
         </div>
