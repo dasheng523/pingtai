27c0160a88f72a46d282b0b5c45b241290022330
dasheng523 <dasheng523@163.com>
2016-03-23 12:23:03 +0800

    调试

--- Application/Phone/Controller/WechatController.class.php
+++ Application/Phone/Controller/WechatController.class.php
@@ -13,15 +13,22 @@ class WechatController extends Controller {
         }
 
         if(IS_POST){
+            \Think\Log::write('微信客户端请求：','DEBUG');
+            \Think\Log::write(print_r($weobj->getRev()->getRevData()),'DEBUG');
+
+
             $type = $weobj->getRev()->getRevType();
             switch($type) {
                 case Wechat::MSGTYPE_TEXT:
                     \Think\Log::write('文本消息','DEBUG');
-                    $weobj->text(getSysConfig('wechat_welcome'))->reply();
-                    exit;
+                    $msg = $weobj->getRev()->getRevContent();
+                    if($msg == 'text'){
+                        $weobj->text('test')->reply();
+                    }
                     break;
                 case Wechat::MSGTYPE_EVENT:
                     \Think\Log::write('事件消息','DEBUG');
+                    $weobj->text(getSysConfig('wechat_welcome'))->reply();
                     break;
                 case Wechat::MSGTYPE_IMAGE:
                     \Think\Log::write('图片消息','DEBUG');
