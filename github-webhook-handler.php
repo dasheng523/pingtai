595ca6c7be809281a8b67f07f512389d40b3fda1
dasheng523 <dasheng523@163.com>
2016-03-24 09:44:18 +0800

    test

--- github-webhook-handler.php
+++ github-webhook-handler.php
@@ -10,6 +10,7 @@ $signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
 if ($signature) {
     $hash = "sha1=".hash_hmac('sha1', $HTTP_RAW_POST_DATA, $secret);
     if (strcmp($signature, $hash) == 0) {
+        echo shell_exec("echo test > log.log");
         echo shell_exec("cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1");
         exit();
     }
