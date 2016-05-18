f8e78d6251fe01221552f7cbf8d79f583a2a81cd
dasheng523 <dasheng523@163.com>
2016-05-18 17:04:19 +0800

    首页跳转

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -3,6 +3,7 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-        header("Location:".UC('Phone/Activity/hotActivity'));
+        //header("Location:".UC('Phone/Activity/hotActivity'));
+        redirect('Phone/Activity/hotActivity', 0, '页面跳转中...');
     }
 }
