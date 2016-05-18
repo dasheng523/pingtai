67fe9f6b506f0769c2a2b6df3ec73c6b630c6cea
dasheng523 <dasheng523@163.com>
2016-05-18 11:45:35 +0800

    修改文案

--- Application/Wechat/Logic/AutoReplyLogic.class.php
+++ Application/Wechat/Logic/AutoReplyLogic.class.php
@@ -588,7 +588,7 @@ class PublishGoodsState{
                     setCurrentHandler($userKey,null);
 
                     $shopUrl = UC('Phone/Miaoji/detail',array('id'=>$id));
-                    return makeText('发布成功!点击<a href="'.$shopUrl.'">查看商品</a>'."\n".'将您的商品分享到朋友圈，可以快速提高人气。\n如需修改，可点击微信菜单中的“店多多”->"商家入口"进行管理。');
+                    return makeText('发布成功!点击<a href="'.$shopUrl.'">查看商品</a>即可看到您的商品。'."\n\n".'将您的商品分享到朋友圈，可以快速提高人气。'."\n\n".'如需修改，可点击微信菜单中的“店多多”->"商家入口"进行管理。');
                 }
             }
 
