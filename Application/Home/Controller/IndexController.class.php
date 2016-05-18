adb3d2243442383fa0ff7330b9e979a704d65950
dasheng523 <dasheng523@163.com>
2016-05-18 17:07:49 +0800

    首页

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -3,7 +3,6 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-        //header("Location:".UC('Phone/Activity/hotActivity'));
-        redirect('Phone/Activity/hotActivity', 0, '页面跳转中...');
+        $this->display();
     }
 }
