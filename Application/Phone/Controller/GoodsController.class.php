7535b6241ae17add544c37199e893eda40f9aefa
dasheng523 <dasheng523@163.com>
2016-05-24 18:13:12 +0800

    修复BUG

--- Application/Phone/Controller/GoodsController.class.php
+++ Application/Phone/Controller/GoodsController.class.php
@@ -112,8 +112,8 @@ class GoodsController extends Controller {
         $data['body'] = $order['title'];
         $data['detail'] = $order['title'];
         $data['out_trade_no'] = $order['order_id'];
-        //$data['total_fee'] = $order['total_fee']*100;
-        $data['total_fee'] = 1;
+        $data['total_fee'] = $order['total_fee']*100;
+        //$data['total_fee'] = 1;
         $data['spbill_create_ip'] = get_client_ip();
         $data['notify_url'] = UC('Public/notifyPay');
         $data['trade_type'] = "JSAPI";
