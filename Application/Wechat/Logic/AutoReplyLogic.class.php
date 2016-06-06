ca13e1ec7722c7910a48fb853311034cc042f6a5
dasheng523 <dasheng523@163.com>
2016-06-06 10:03:05 +0800

    优化自动回复

--- Application/Wechat/Logic/AutoReplyLogic.class.php
+++ Application/Wechat/Logic/AutoReplyLogic.class.php
@@ -283,8 +283,8 @@ class MainState{
 class OpenShopState{
     public function handle($msg){
         $userKey = getUserKey($msg);
-
-        if($msg == 0){
+        $text = getMsgContent($msg);
+        if($text == 0){
             $this->resetStep($userKey);
             return makeText("已取消开店\n如果您准备好了，回复 A 即可继续开店\n\n回复M 打开主菜单");
         }
